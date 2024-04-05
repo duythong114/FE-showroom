fc525460ac36ede86907119bfc4c66f6cea09ac1
duythong114 <tranthong.facebook@gmail.com>
2024-04-05 19:41:31 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello duy thong</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+}
+
+export default App;
