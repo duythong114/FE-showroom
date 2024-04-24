c8b3f81cd322da4006e0c7896439105426eb8991
duythong114 <tranthong.facebook@gmail.com>
2024-04-24 15:18:46 +0700

    fix private route

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -3,10 +3,21 @@ import {
 } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { Redirect } from "react-router-dom";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const PrivateRoute = (props) => {
+    // const isRefreshingUser = useSelector(state => state.user.isRefreshingUser)
     const isAuthenticated = useSelector(state => state.user.isAuthenticated)
     const user = useSelector(state => state.user.user)
+    const history = useHistory()
+
+    useEffect(() => {
+        if (!(isAuthenticated || user)) {
+            history.push('/login')
+        }
+        // eslint-disable-next-line
+    }, [])
 
     if (isAuthenticated && user) {
         return (
